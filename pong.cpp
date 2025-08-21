4ee4e75bd27fb7ecf10caf7aefc6e7453fc806c2
Deniz Deveci <dodeve@gmail.com>
2025-08-21 15:30:06 +0900

    added pong

--- pong.cpp
+++ pong.cpp
@@ -1,27 +1,81 @@
 #include <raylib.h>
+class Ball {
+public:
+  float x, y;
+  int speedX, speedY;
+  int radius;
+
+  void Draw() { DrawCircle(x, y, radius, WHITE); }
+  void Update() {
+    x += speedX;
+    y += speedY;
+    if (y + radius >= GetScreenHeight() || y - radius <= 0)
+      speedY *= -1;
+    if (x + radius >= GetScreenWidth() || x - radius <= 0)
+      speedX *= -1;
+  }
+};
+
+class Paddle {
+public:
+  float x, y;
+  float width, height;
+  int speed;
+
+  void Draw() {
+
+    DrawRectangle(
+        x, y, width, height,
+        WHITE); // Rectangle is drawn its topleft point just like the window
+  };
+  void Update() {
+    if (IsKeyDown(KEY_UP))
+      y -= speed;
+    if (IsKeyDown(KEY_DOWN))
+      y += speed;
+    if (y <= 0)
+      y = 0;
+    if (y + height >= GetScreenHeight())
+      y = GetScreenHeight() - height;
+  }
+};
+
+Ball ball;
+Paddle player;
 
 int main() {
-  int ballX = 400;
-  int ballY = 400;
   Color green = {20, 160, 133, 255};
-  InitWindow(800, 800, "The Window");
+  // The paddles
+  const int screenWidth = 1280;
+  const int screenHeight = 800;
+  const int rectangleHeight = 120;
+  const int rectangleWidth = 25;
+  // Ball attributes
+  ball.x = screenWidth / 2;
+  ball.y = screenHeight / 2;
+  ball.speedX = 7;
+  ball.speedY = 7;
+  ball.radius = 20;
+
+  // Player attributes
+  player.width = 25;
+  player.height = 120;
+  player.x = player.width - 10;
+  player.y = screenHeight / 2 - player.height / 2;
+  player.speed = 6;
+  InitWindow(screenWidth, screenHeight, "The pong");
   SetTargetFPS(60);
   while (!WindowShouldClose()) {
-
-    if (IsKeyPressed(KEY_DOWN))
-      ballY += 3;
-    else if (IsKeyPressed(KEY_UP))
-      ballY -= 3;
-    if (IsKeyPressed(KEY_RIGHT))
-      ballX += 3;
-    else if (IsKeyPressed(KEY_LEFT))
-      ballX -= 3;
-
-    BeginDrawing();
-    ClearBackground(green);
-    DrawCircle(ballX, ballY, 50, WHITE);
-
-    EndDrawing();
+    BeginDrawing(); // Begin canvas drawing
+    ClearBackground(BLACK);
+    ball.Draw();
+    ball.Update();
+    player.Draw();
+    player.Update();
+    DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
+    DrawRectangle(screenWidth - 35, (screenHeight - rectangleHeight) / 2,
+                  rectangleWidth, rectangleHeight, WHITE);
+    EndDrawing(); // End canvas drawing
   }
 
   CloseWindow();
