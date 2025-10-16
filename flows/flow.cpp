d56707038171b32292bfc8c973f8f6cb47e8dbd4
Deniz Deveci <dodeve@gmail.com>
2025-10-17 01:11:27 +0900

    working zoom

--- flows/flow.cpp
+++ flows/flow.cpp
@@ -7,6 +7,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <raylib.h>
+#include <string>
 #include <sys/types.h>
 #include <time.h>
 
@@ -15,32 +16,12 @@ int main() {
   InitWindow(HEIGHT, WIDTH, "Vector PLots");
   SetTargetFPS(FPS);
 
-  float xRange{4.0f}; // x will range from -4 to 4 but then changed by scrolling
-  float step{xRange * 2 / wavePoints}; // step size for plotting
-
-  for (std::size_t y{0}; y < static_cast<std::size_t>(wavePoints); ++y) {
-    for (std::size_t x{0}; x < static_cast<std::size_t>(wavePoints); ++x) {
-      slot s;
-      s.start_point = {
-          static_cast<float>(-xRange + x * step),
-          static_cast<float>(-xRange + y * step)}; // (float is C-style cast!)
-      BOARD[y][x] = s;
-    }
-  }
-
   // get the max length for coloring purposes
   // double max_length{getMax(BOARD, COLS, ROWS)};
 
-  // Particle creation
-  std::vector<Particle> particles;
-  for (std::size_t i{0}; i < NUM_PARTICLES; ++i) {
-    Particle p{
-        RealVector(Random::get(1, xRange - 1), Random::get(1, xRange - 1)),
-        RealVector(0, 0), PARTICLE_MIN_SPEED,
-        PARTICLE_MAX_SPEED}; // generates random positions for
-    particles.push_back(p);
-  }
-  std::cout << "step size is" << step << '\n';
+  float xRange{4.0f}; // x will range from -4 to 4 but then changed by scrolling
+  float step{xRange * 2 / wavePoints}; // step size for plotting
+  float length{step / 2};              // length of the vectors we draw
 
   while (!WindowShouldClose()) {
 
@@ -57,13 +38,25 @@ int main() {
     if (xRange > 50.0f)
       xRange = 50.0f;
 
+    step = xRange * 2 / wavePoints;
+    length = step / 1.5;
+
+    // start points set by variable step size
+    for (std::size_t y{0}; y < static_cast<std::size_t>(wavePoints); ++y) {
+      for (std::size_t x{0}; x < static_cast<std::size_t>(wavePoints); ++x) {
+        slot s;
+        s.start_point = {static_cast<float>(-xRange + x * step),
+                         static_cast<float>(-xRange + y * step)};
+        BOARD[y][x] = s;
+      }
+    }
+
     // Draw axes
-    // DrawLine(WIDTH / 2, 0, WIDTH / 2, HEIGHT, GRAY);
-    // DrawLine(0, HEIGHT / 2, WIDTH, HEIGHT / 2, GRAY);
+    DrawLine(WIDTH / 2, 0, WIDTH / 2, HEIGHT, GRAY);
+    DrawLine(0, HEIGHT / 2, WIDTH, HEIGHT / 2, GRAY);
 
     DrawText("Y", WIDTH / 2 + 5, 5, 20, GRAY);
     DrawText("X", WIDTH - 20, HEIGHT / 2 + 5, 20, GRAY);
-    float step{xRange * 2 / wavePoints}; // step size for plotting
 
     for (std::size_t y{0}; y < static_cast<std::size_t>(wavePoints); ++y) {
       for (std::size_t x{0}; x < static_cast<std::size_t>(wavePoints); ++x) {
@@ -84,40 +77,7 @@ int main() {
         DrawLineEx(projectedStart, projectedEnd, 1, (Color){0, 255, 0, 255});
       }
     }
-    for (std::size_t i{0}; i < NUM_PARTICLES; ++i) {
-      RealVector mv = BOARD[static_cast<int>(particles[i].pos.y / step)]
-                           [static_cast<int>(particles[i].pos.x / step)]
-                               .vec;
-      // particles[i].applyForce(mv);
-      // std::cout << particles[i].pos.x << '\n';
-      // particles[i].update();
-      particles[i].show();
-    }
 
-    // for (int y = 0; y < ROWS; ++y) {
-    //   for (int x = 0; x < COLS; ++x) {
-    //     double x_component{getQuadrant(x, y, HALF_CELLS, BOARD).x};
-    //     double y_component{getQuadrant(x, y, HALF_CELLS, BOARD).y};
-    //     double angle = field_func(x_component, y_component);
-    //     BOARD[y][x].vec = RealVector(cos(angle) * length, sin(angle) *
-    //     length);
-    //     Vector2 end = {BOARD[y][x].start_point.x + BOARD[y][x].vec.x,
-    //     BOARD[y][x].start_point.y + BOARD[y][x].vec.y}; double magnitude{
-    //     std::abs(RealVector(x_component, y_component).getMag())};
-    //     // u_char c = map_to(0, max_length, 0, 255, magnitude);
-    //
-    //     DrawLineEx(BOARD[y][x].start_point, end, 2, (Color){0, 255, 0,
-    //     255});
-    //   }
-    // }
-    // for (std::size_t i{0}; i < NUM_PARTICLES; ++i) {
-    //   RealVector mv = BOARD[static_cast<int>(particles[i].pos.y / SCL)]
-    //                        [static_cast<int>(particles[i].pos.x / SCL)]
-    //                            .vec;
-    //   particles[i].applyForce(mv);
-    //   particles[i].update();
-    //   particles[i].show();
-    // }
     // charged object drawing
     Vector2 charge_start = {projectedVector(-1, 0, xRange)};
     Vector2 charge_end = {projectedVector(1, 0, xRange)};
